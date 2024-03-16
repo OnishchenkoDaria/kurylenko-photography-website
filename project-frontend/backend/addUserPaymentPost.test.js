5a65a07008b302a79228e182732b715aa616f33a
Daria Onishchenko <daria.onishchenko.57@gmail.com>
2024-03-17 00:01:36 +0200

    changed addPayment unit tests (removed unused variables and added console logs) -- passes successfully
    this and previous commits changes were manually checked -- payment system works perfect

--- project-frontend/backend/addUserPaymentPost.test.js
+++ project-frontend/backend/addUserPaymentPost.test.js
@@ -6,37 +6,38 @@ jest.mock('./setTimeDate', () => ({
 }))
 
 describe('AddUserPayment function', () => {
-    let res, req, json, status, query, err, result
+    let query, err
 
     beforeEach(() => {
         price = '2'
         user_email = 'test@gmail.com'
-        
-        req={}
-
-        status = jest.fn().mockReturnThis();
-        json = jest.fn();
-        res = { status, json };
 
         query = jest.fn();
         db.query = query;
 
         err = null;
-        result = [];
     })
 
     afterEach(() => {
         jest.clearAllMocks();
     })
 
+    //mocking console prints
+    jest.spyOn(console, 'log').mockImplementation(() => {})
+    jest.spyOn(console, 'error').mockImplementation(() => {})
+
     test('should return error of fail query execution', () => {
         const err = new Error('Insert error')
         query.mockImplementation((_, __, callback) => callback(err))
         AddUserPayment(user_email, price)
+
+        expect(console.error).toHaveBeenCalledWith('error: ', err)
     })
 
     test('should return success status of insert query', () => {
         query.mockImplementation((_, __, callback) => callback(null))
         AddUserPayment(user_email, price)
+
+        expect(console.log).toHaveBeenCalledWith('payment added!')
     })
 })
