4c9405901af739a86da684021cc6cdb49f2f0c38
OnishchenkoDaria <daria.onishchenko.57@gmail.com>
2024-07-19 16:37:10 +0300

    *updated forntend with the tailwind *updated the login page view

--- project-frontend/frontend/src/components/LoginForm.jsx
+++ project-frontend/frontend/src/components/LoginForm.jsx
@@ -9,6 +9,7 @@ import { Link } from "react-router-dom";
 import { Icon } from "react-icons-kit";
 import { eyeOff } from "react-icons-kit/feather/eyeOff";
 import { eye } from "react-icons-kit/feather/eye";
+import { user } from "react-icons-kit/feather/user";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
@@ -63,54 +64,69 @@ const LoginForm = () => {
 
   return (
     <>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Col lg={10}>
-            <Form.Control
+
+    <div className="backdrop-blur-md rounded-lg shadow-xl bg-white/30 p-9 ">
+
+    <div className="border-b-2">
+      <p className="text-white text-4xl pb-6 text-center">Log In</p>
+    </div>
+
+      <form onSubmit={handleSubmit} className="w-full max-w-sm pt-6 text-center">
+
+        <label className="block text-white tracking-wider mb-2 md:text-left">Email address</label>
+        
+        <div className="md:flex md:items-center mb-6">
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-neutral-700 mb-3"
               name="email"
               value={formInput.email}
               onChange={handleChange}
-              placeholder="Email"
+              placeholder="user@email.com"
               required
             />
-          </Col>
-        </Form.Group>
+            <span
+                className="flex justify-around items-center"
+              >
+                <Icon className="pl-3 pb-3 text-white" icon={user} size={25} />
+            </span>
+        </div>
+        
+        <label className="block text-white tracking-wider pr-1 mb-2 md:text-left">Password</label>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Row>
-            <Col xs={10}>
-              <Form.Control
+        <div className="md:flex md:items-center mb-6">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-neutral-700 mb-3"
                 type={type}
                 name="password"
                 value={formInput.password}
                 onChange={handleChange}
-                placeholder="Password"
+                placeholder="password"
                 autoComplete="off"
                 required
               />
-            </Col>
-            <Col xs={1}>
               <span
-                className="flex justify-around items-center"
+                className="flex justify-around items-center cursor-pointer"
                 onClick={toggleVisibility}
               >
-                <Icon className="absolute mr-10" icon={icon} size={25} />
+                <Icon className="pl-3 pb-3 text-white" icon={icon} size={25} />
               </span>
-            </Col>
-          </Row>
-        </Form.Group>
-        <Button variant="dark" type="submit">
-          Submit
-        </Button>
-        <p className="mt-3">
-          Do not have an account yet?
-          <br />
-          <Link to={PathConstants.REGISTRATION}>Register now</Link>
-        </p>
-      </Form>
+        </div>
+
+        <div className="flex justify-around items-center m-3">
+          <button className="tracking-wider text-white text-lg shadow hover:outline py-2 px-4 rounded" type="submit">
+            Submit
+          </button>
+        </div>
+        <div className="flex justify-around items-center">
+          <p className="text-white text-center text-lg leading-relaxed mt-3">
+            Do not have an account yet?
+            <br />
+            <Link to={PathConstants.REGISTRATION} className="underline hover:text-neutral-700">Register now</Link>
+          </p>
+        </div>
+      </form>
       {message && <p className="text-danger">{message}*</p>}
+      </div>
     </>
   );
 };
--- project-frontend/frontend/src/pages/Login.jsx
+++ project-frontend/frontend/src/pages/Login.jsx
@@ -3,6 +3,7 @@ import LoginForm from "../components/LoginForm";
 import axios from 'axios';
 import PathConstants from '../routes/pathConstants';
 import { useNavigate } from 'react-router-dom'
+import back_pic from '../assets/photo1.jpg'
 
 const Login = () => {
   const navigate = useNavigate();
@@ -10,8 +11,13 @@ const Login = () => {
   .then(()=>navigate(PathConstants.ACCOUNT))
   .catch((err)=>console.log('session exist'))
   return (
-    <div className="d-flex justify-content-center mt-5">
-      <LoginForm />
+    <div 
+      className="min-h-screen bg-cover bg-center overflow-hidden" 
+      style={{ backgroundImage: `url(${back_pic})` }}
+    >
+      <div className="bg-black bg-opacity-50 min-h-screen flex justify-center items-center">
+        <LoginForm />
+      </div>
     </div>
   );
 };
