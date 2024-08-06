ce0983ff49c42d555ba6b99f1293df9dcead1e51
OnishchenkoDaria <daria.onishchenko.57@gmail.com>
2024-08-06 23:34:41 +0300

    reorganisation: renamed previous payment component into the CustomPhotoshoot
    TO DO: new Payment Page content (purchase plans)

--- project-frontend/frontend/src/pages/Purchase.jsx
+++ project-frontend/frontend/src/pages/Purchase.jsx
@@ -7,28 +7,11 @@ import { useNavigate } from 'react-router-dom'
 
 // flex + justify-center (center for X) + items-center (center for Y)
 
-const Purchase = () => {
-  
-  //unable the not registered users to buy the phohtoshoot
-   const navigate = useNavigate();
-  axios.post('http://localhost:3001/users/session-hook')
-  .then((par)=>{
-    console.log('welcome', par);
-  })
-  .catch(() => {
-    navigate(PathConstants.LOGIN);
-  })
+const Purchase = () => {  
   
   return (
-    <div 
-      className="bg-cover bg-center bg-no-repeat bg-fixed min-h-screen"
-      style={{ backgroundImage: `url(${back_pic})` }}
-    >
-      <div className='backdrop-blur-md bg-black/30 min-h-screen text-white text-center tracking-wide'>
-        <p className='text-4xl pt-8 pb-4 border-b-2 border-gray-400/20 w-auto inline-block'>Set your photoshoot</p>
-        <p className='text-xl mt-3 mb-8'>Select every block with the radio buttons necessarily</p>
-        <Payment />
-      </div>
+    <div>
+        hi :D
     </div>
   );
 }
